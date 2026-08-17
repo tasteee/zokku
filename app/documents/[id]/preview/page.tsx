2d2edd15fbc15e97ae89a02bde3045a3a9ff479e
shane <shanecolcleasure@gmail.com>
2026-08-17 17:00:33 -0500

    Use local IDs in preview route

--- app/documents/[id]/preview/page.tsx
+++ app/documents/[id]/preview/page.tsx
@@ -1,12 +1,5 @@
 import { DocumentPreview } from '@/components/DocumentPreview'
-import type { Id } from '@/convex/_generated/dataModel'
 import { JSX } from 'react'
-
-// This will eventually become gated by a documents
-// visibility setting, being public or private.
-// For now, it requires auth to get this far,
-// so it is technically all documents are private
-// by default.
 
 type DocumentPreviewPagePropsT = {
 	params: Promise<{ id: string }>
@@ -14,9 +7,7 @@ type DocumentPreviewPagePropsT = {
 
 const DocumentPreviewPage = async (props: DocumentPreviewPagePropsT): Promise<JSX.Element> => {
 	const params = await props.params
-	const documentId = params.id as Id<'documents'>
-
-	return <DocumentPreview documentId={documentId} />
+	return <DocumentPreview documentId={params.id} />
 }
 
 export default DocumentPreviewPage
