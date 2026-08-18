8750c3ab937a31b94ed60d1e6cab0acdebe2900a
shane <shanecolcleasure@gmail.com>
2026-08-17 19:24:15 -0500

    Simplify documents header

--- app/documents/components/DocumentsHeader/DocumentsHeader.tsx
+++ app/documents/components/DocumentsHeader/DocumentsHeader.tsx
@@ -1,24 +1,13 @@
 import './DocumentsHeader.css'
 
 import { JSX } from 'react'
-import { ZButton } from '@/components/zButton'
 import { ZokkuBrand } from '@/components/ZokkuBrand'
 
-type DocumentsHeaderPropsT = {
-	onSignOut: () => void
-}
-
-export const DocumentsHeader = (props: DocumentsHeaderPropsT): JSX.Element => {
+export const DocumentsHeader = (): JSX.Element => {
 	return (
 		<header className="documentsHeader">
 			<div className="documentsHeaderBrand">
 				<ZokkuBrand />
-			</div>
-
-			<div className="documentsHeaderActions">
-				<ZButton isSmall isOutlined isNeutral onClick={props.onSignOut}>
-					Change workspace
-				</ZButton>
 			</div>
 		</header>
 	)
